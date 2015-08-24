b4e5c60ae9d7f7c50c89d2ffc976db6ecd43a04f
YangCun <yangcun@live.com>
2015-08-24 14:29:20 +0800

    call an instance func from another class file

---
+++ main.py
@@ -0,0 +1,14 @@
+from subtitle_adjustment import SubtitleAdjustment
+    
+def main():
+    print("starting")
+    
+    SubtitleAdjustment.test()
+
+    sa = SubtitleAdjustment()
+    sa.test2()
+
+    sa.run()
+
+if __name__ == "__main__":
+    main()
